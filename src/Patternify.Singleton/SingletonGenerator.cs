718aa74af06e969ea9b9a138623979518fd53fcb
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2023-11-01 01:49:50 +0100

    Dodaj pliki projektów.

---
+++ src/Patternify.Singleton/SingletonGenerator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Patternify.Singleton;
+
+[Generator]
+internal sealed class SingletonGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+    }
+}
